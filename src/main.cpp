f5b7619e66c00fb7161aa6bad9a3cce0bac4e111
Sheldn-078 <hengrui078@gmail.com>
2026-08-14 21:15:44 +0800

    calculator update

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,38 @@
-#include<iostream>
+#include <iostream>
+#include "calculator.h"
 
-int main(){
-	int a = 10;
-    int b = 20;
 
-    int sum = a + b;
+int main()
+{
 
-    std::cout << "a = " << a << std::endl;
-    std::cout << "b = " << b << std::endl;
-    std::cout << "sum = " << sum << std::endl;
+    int a;
+    int b;
+
+
+    std::cout << "Please enter a: ";
+    std::cin >> a;
+
+
+    std::cout << "Please enter b: ";
+    std::cin >> b;
+
+
+
+    std::cout << "sum = "
+              << add(a,b)
+              << std::endl;
+
+
+    std::cout << "difference = "
+              << subtract(a,b)
+              << std::endl;
+
+
+    std::cout << "product = "
+              << multiply(a,b)
+              << std::endl;
+
+
 
     return 0;
 }
